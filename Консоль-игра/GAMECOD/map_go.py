58364c483d8d07e5440e189a7a38012ce6459c7b
Pse1vdonu <mihailcepanov0@gmail.com>
2026-08-26 17:40:50 +0300

    Delete Консоль-игра directory

--- Консоль-игра/GAMECOD/map_go.py
+++
@@ -1,5 +0,0 @@
-from .move import move_up
-
-def go():
-    for i in move_up():
-        print(''.join(i))
--- Консоль-игра/GAMECOD/strok.py
+++
@@ -1,9 +0,0 @@
-from .color import q, e
-
-strok1=[q, q, q, q, q, q]
-strok2=[q, q, q, q, q, q]
-strok3=[q, q, q, q, q, q]
-strok4=[q, q, q, q, q, q]
-strok5=[q, q, q, q, q, q]
-strok6=[q, q, q, e, q, q]
-strok7=[q, q, q, q, q, q]
